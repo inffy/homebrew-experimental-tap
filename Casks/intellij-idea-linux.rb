b7cb2c3fc111b29927ae503cf6f5dd8af3418dea
Ira Limitanei <47409392+lambdaclan@users.noreply.github.com>
2026-02-21 04:32:00 +0000

    intellij-idea-linux 2025.3.3

--- Casks/intellij-idea-linux.rb
+++ Casks/intellij-idea-linux.rb
@@ -3,9 +3,9 @@ cask "intellij-idea-linux" do
        arm:   "-aarch64"
   os linux: "linux"
 
-  version "2025.3.1.1,253.29346.240"
-  sha256 x86_64_linux: "3a064b22961f3f39b866b64b628558e0d0f708d423a3f9565d43f0e81196997b",
-         arm64_linux:  "87516d2f07b8ec1ff6cfe9d14d68fc3f76f5d57b86611325b9eabac1b6043ccb"
+  version "2025.3.3,253.31033.145"
+  sha256 x86_64_linux: "19fdadc1dca1f76624920903c445da4ab23267c81119353ceba899736de2f118",
+         arm64_linux:  "6e435b9e39dee4fd1946470ed911694c312c1b6c1fa5e345bcd48fbe13594dc8"
 
   url "https://download.jetbrains.com/idea/ideaIU-#{version.csv.first}#{arch}.tar.gz"
   name "IntelliJ IDEA Ultimate"
